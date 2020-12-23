c1f3e82a4a0196ce67d2666cb83414cfa3a0e00c
MaksymKapelianovych <vfrc.zomby30@gmail.com>
2020-12-23 19:45:17 +0200

    Added faculty page for admin

---
+++ src/main/java/ua/vstup/command/impl/admin/FacultiesPageCommand.java
@@ -0,0 +1,20 @@
+package ua.vstup.command.impl.admin;
+
+import ua.vstup.annotation.CommandMapping;
+import ua.vstup.command.Command;
+import ua.vstup.command.impl.AbstractCommand;
+import ua.vstup.constantutils.Constants;
+import ua.vstup.domain.Faculty;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@CommandMapping(url = Constants.Urls.ADMIN_FACULTY_FORWARD)
+public class FacultiesPageCommand extends AbstractCommand implements Command {
+    @Override
+    public String execute(HttpServletRequest request) {
+        List<Faculty> faculties = getFacultyService(request).getAll();
+        request.setAttribute(Constants.Attributes.FACULTY_LIST, faculties);
+        return "/admin/faculty/faculties.jsp";
+    }
+}
